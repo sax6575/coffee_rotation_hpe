fea845b180a1918e4b0c037965439b76fd1ab96c
sax6575 <sax6575@fastmail.com>
2025-03-10 17:14:18 -0600

    Building out CoffeeOrder model

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## Coffee Payment Rotation Project
 
 ### Contact
-josh.t.sachs@gmail.com
-Joshua Sachs
+
+- josh.t.sachs@gmail.com
+- Joshua Sachs
+
 [3/10/25]
--- app/src/main/groovy/coffeerotation/model/CoffeeOrder.groovy
+++ app/src/main/groovy/coffeerotation/model/CoffeeOrder.groovy
@@ -1,4 +1,37 @@
 package coffeerotation.model
 
 class CoffeeOrder {
+
+    String id
+    Date orderDate
+    List<Coworker> participants = []
+    Coworker paidBy
+    BigDecimal totalCost
+
+    CoffeeOrder(Date orderDate, List<Coworker> participants, Coworker paidBy) {
+        this.id = UUID.randomUUID().toString()
+        this.orderDate = orderDate
+        this.participants = participants
+        this.paidBy = paidBy
+        calculateTotalCost()
+    }
+
+    private void calculateTotalCost() {
+        this.totalCost = participants.sum { it.beveragePrice } ?: 0 as BigDecimal
+    }
+
+    void recordPayment() {
+        paidBy.recordPayment(totalCost, orderDate)
+    }
+
+    Map<String, BigDecimal> getItemizedCosts() {
+        return participants.collectEntries { coworker ->
+            [(coworker.name): coworker.beveragePrice]
+        }
+    }
+
+    @Override
+    String toString() {
+        return "Coffee order on ${orderDate.format('yyyy-MM-dd')} - Paid by: ${paidBy.name}, Total: \$${totalCost}"
+    }
 }
